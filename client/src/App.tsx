55b07221e3089e8424833e88354b755908048af5
islam <lameche1islam@gmail.com>
2025-07-19 21:08:20 +0100

    Refactor App component to use react-router for navigation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
-import Board from "./components/Board";
-import BoardContextProvider from "./context/BoardContextProvider";
-
+import { BrowserRouter, Route, Routes } from "react-router";
+import BoardPage from "./routes/BoardPage";
+import CreateOrJoin from "./routes/CreateOrJoin";
 function App() {
   return (
-    <BoardContextProvider>
-      <Board />
-    </BoardContextProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<CreateOrJoin />} />
+        <Route path="/game/:roomId" element={<BoardPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 export default App;
