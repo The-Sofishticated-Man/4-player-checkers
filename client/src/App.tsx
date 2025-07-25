49a027370b8c6e9de67b70231a626e45916ed78d
islam <lameche1islam@gmail.com>
2025-07-25 19:15:33 +0100

    Wrap App component with SocketProvider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import BoardPage from "./routes/BoardPage";
 import CreateOrJoin from "./routes/CreateOrJoin";
+import { SocketProvider } from "./context/SocketProvider";
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<CreateOrJoin />} />
-        <Route path="/game/:roomId" element={<BoardPage />} />
-      </Routes>
-    </BrowserRouter>
+    <SocketProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<CreateOrJoin />} />
+          <Route path="/game/:roomId" element={<BoardPage />} />
+        </Routes>
+      </BrowserRouter>
+    </SocketProvider>
   );
 }
+
 export default App;
