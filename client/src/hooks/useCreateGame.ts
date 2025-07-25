fda3bb224ab51f63f5c09630615fed8714de11fb
islam <lameche1islam@gmail.com>
2025-07-25 19:15:36 +0100

    Refactor useCreateGame hook and improve logic

--- client/src/hooks/useCreateGame.ts
+++ client/src/hooks/useCreateGame.ts
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router";
 import { useSocket } from "./useSocket";
-import initialBoard from "../utils/initialState";
+import initialState from "../utils/initialState";
 
 export function useCreateGame() {
   const navigate = useNavigate();
@@ -8,23 +8,13 @@ export function useCreateGame() {
 
   const createGame = (afterCreate?: () => void) => {
     // Get or create persistent player ID
-    let playerId = localStorage.getItem("playerId");
-    if (!playerId) {
-      playerId = Math.random().toString(36).substring(2, 15);
-      localStorage.setItem("playerId", playerId);
-    }
+    socket?.emit("create-room", initialState.checkersBoardState);
 
-    socket?.emit("create-room", initialBoard, playerId);
-
-    socket?.on(
-      "room-created",
-      (data: { roomID: string; boardState: number[][]; playerId: string }) => {
-        localStorage.setItem("playerId", data.playerId);
-        localStorage.setItem("currentRoomId", data.roomID);
-        navigate(`/game/${data.roomID}`);
-        afterCreate?.();
-      }
-    );
+    socket?.on("room-created", ({ roomID }: { roomID: string }) => {
+      // todo: implement some way of letting player join multiple games
+      navigate(`/game/${roomID}`);
+      afterCreate?.();
+    });
   };
 
   return { createGame };
