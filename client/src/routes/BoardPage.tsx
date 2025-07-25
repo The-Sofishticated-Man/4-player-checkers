49ac4a41b20b02057fd4e5c868a95897803cc5d1
islam <lameche1islam@gmail.com>
2025-07-25 20:56:07 +0100

    Refactor BoardPage to use content component

--- client/src/routes/BoardPage.tsx
+++ client/src/routes/BoardPage.tsx
@@ -2,15 +2,22 @@ import { useParams } from "react-router";
 import Board from "../components/Board";
 import BoardContextProvider from "../context/BoardContextProvider";
 import { useJoinGame } from "../hooks/useJoinGame";
+import initialState from "../utils/initialState";
+
+function BoardPageContent() {
+  const { roomId } = useParams();
+  useJoinGame(roomId!); // This will handle socket events and dispatch updates
+
+  return <Board />;
+}
 
 function BoardPage() {
   const { roomId } = useParams();
   console.log("BoardPage loaded with roomId:", roomId);
-  const { initialStateFromServer } = useJoinGame(roomId!);
 
   return (
-    <BoardContextProvider initialStateFromServer={initialStateFromServer!}>
-      <Board/>
+    <BoardContextProvider initialStateFromServer={initialState}>
+      <BoardPageContent />
     </BoardContextProvider>
   );
 }
