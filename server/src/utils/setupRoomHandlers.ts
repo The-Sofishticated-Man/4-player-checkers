f84df6b6c8070dc786e295790921e9a6981771ea
islam <lameche1islam@gmail.com>
2025-10-04 16:26:24 +0100

    Fix import casing in setupRoomHandlers

--- server/src/utils/setupRoomHandlers.ts
+++ server/src/utils/setupRoomHandlers.ts
@@ -1,7 +1,6 @@
 import { Socket } from "socket.io";
-import { generateRoomId } from "./gameUtils.ts";
 import { Game } from "../models/Game.ts";
-import { RoomHandlers } from "../handlers/RoomHandlers.ts";
+import { RoomHandlers } from "../handlers/roomHandlers.ts";
 
 export const setupRoomHandlers = (socket: Socket, games: Map<string, Game>) => {
   // Handle room creation
